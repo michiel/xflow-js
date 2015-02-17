fbd260685a2e2f4f5ae74d4f11011863c3870f54
Michiel Kalkman <michiel@nosuchtype.com>
2015-02-17 21:06:11 +0100

    Bump wip

---
+++ test/xflow_test.js
@@ -0,0 +1,9 @@
+var should = require('chai').should();
+var xflow  = require('../lib/xflow');
+
+describe('xflow', function() {
+    it('loads a json flow', function() {
+        xflow().should.equal('');
+      });
+  });
+

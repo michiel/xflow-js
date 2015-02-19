9a56e28ea37251fcdf0a500fb6cdfcdf3d529d3e
Michiel Kalkman <michiel@nosuchtype.com>
2015-02-19 06:28:32 +0100

    Add simple tests

--- test/xflow_test.js
+++ test/xflow_test.js
@@ -1,9 +1,28 @@
-var should = require('chai').should();
+var chai           = require('chai');
+var should         = chai.should();
+var chaiAsPromised = require("chai-as-promised");
+
+chai.use(chaiAsPromised);
+
 var xflow  = require('../lib/xflow');
 
-describe('xflow', function() {
+var fs = require('fs');
+
+describe('xflow sync ', function() {
     it('loads a json flow', function() {
-        xflow().should.equal('');
+        var data = fs.readFileSync('data/create_object.json', 'utf-8');
+        var json = JSON.parse(data);
+        var res = xflow(json, {}).start();
+        res.should.equal(true);
+      });
+  });
+
+describe('xflow async ', function() {
+    it('loads a json flow', function() {
+        var data = fs.readFileSync('data/create_object.json', 'utf-8');
+        var json = JSON.parse(data);
+        var res = xflow(json, {}).startQ();
+        res.should.eventually.equal(true);
       });
   });
 

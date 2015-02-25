54555ec06c05f345ab2d4dc55d2c0de91de0c66a
Michiel Kalkman <michiel@nosuchtype.com>
2015-02-25 15:45:08 +0100

    ES6ify xflowExecutor class

--- test/unit/xflow_test.js
+++ test/unit/xflow_test.js
@@ -11,14 +11,14 @@ describe('xFlow sync ', function() {
     it('loads a json flow', function() {
         var data = fs.readFileSync('data/create_object.json', 'utf-8');
         var json = JSON.parse(data);
-        var res = xFlow(json, {}).start();
+        var res = (new xFlow(json, {})).start();
         expect(res).to.deep.equal([{}]);
       });
 
     it('runs a flow with an arithmetic expression ', function() {
         var data = fs.readFileSync('data/arithmetic_addition.json', 'utf-8');
         var json = JSON.parse(data);
-        var res = xFlow(json, {}).start();
+        var res = (new xFlow(json, {})).start();
 
         expect(JSON.stringify(res)).to.equal(
           JSON.stringify([{
@@ -33,14 +33,14 @@ describe('xFlow async ', function() {
     it('loads a json flow', function() {
         var data = fs.readFileSync('data/create_object.json', 'utf-8');
         var json = JSON.parse(data);
-        var res = xFlow(json, {}).startQ();
+        var res = (new xFlow(json, {})).startQ();
         expect(res).to.eventually.deep.equal([{}]);
       });
 
     it('runs a flow with an arithmetic expression ', function() {
         var data = fs.readFileSync('data/arithmetic_addition.json', 'utf-8');
         var json = JSON.parse(data);
-        var res = xFlow(json, {}).startQ();
+        var res = (new xFlow(json, {})).startQ();
         expect(res).to.eventually.deep.equal([{
             'ReturnValue' : 3
           }]);

919104b4b6c636ffa854490a195d54c1c8c98c08
Michiel Kalkman <michiel@nosuchtype.com>
2015-02-27 13:11:31 +0100

    Add xflow validator, tests

--- test/unit/data_test.js
+++ test/unit/data_test.js
@@ -3,14 +3,21 @@ import chaiAsPromised from 'chai-as-promised';
 import fs from 'fs';
 import tv4 from 'tv4';
 
+import xFlowValidator from '../../lib/xflow-validator';
+
 chai.use(chaiAsPromised);
 
-var flowFiles = [
+var goodFlowFiles = [
   'data/branch_boolean.json',
   'data/create_object.json',
-  'data/arithmetic_addition.json',
+  'data/arithmetic_addition.json'
+];
+
+var badFlowFiles = [
   'data/bad_flows/multiple_entry_nodes.json'
 ];
+
+var allFlowFiles = goodFlowFiles.concat(badFlowFiles);
 
 var schemaFile = 'data/xflow-schema.json';
 var schemaJSON = fs.readFileSync(schemaFile, 'utf-8');
@@ -18,7 +25,7 @@ var schemaJSON = fs.readFileSync(schemaFile, 'utf-8');
 describe('JSON data ', function() {
   it(' all flows should validate ', function() {
 
-    flowFiles.forEach(function(file) {
+    allFlowFiles.forEach(function(file) {
       var data = fs.readFileSync(file, 'utf-8');
       var json = JSON.parse(data);
       var res  = tv4.validate(json, schemaJSON);
@@ -27,4 +34,28 @@ describe('JSON data ', function() {
   });
 
 });
+
+describe('JSON data - graph validation ', function() {
+
+  it(' all good flows should validate ', function() {
+    goodFlowFiles.forEach(function(file) {
+      var data = fs.readFileSync(file, 'utf-8');
+      var json = JSON.parse(data);
+      var res  = (new xFlowValidator(json, schemaJSON)).validate();
+      expect(res).to.equal(true);
+    });
+  });
+
+  it(' all bad flows should FAIL to validate ', function() {
+    badFlowFiles.forEach(function(file) {
+      var data = fs.readFileSync(file, 'utf-8');
+      var json = JSON.parse(data);
+      var res  = (new xFlowValidator(json, schemaJSON)).validate();
+      expect(res).to.equal(false);
+    });
+  });
+
+});
+
+
 

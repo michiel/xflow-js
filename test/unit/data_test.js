9ae02635d2d34b66f55539bb05ecef7469e48910
Michiel Kalkman <michiel@nosuchtype.com>
2015-03-17 10:14:39 +0100

    Update and tweak JSON validation

--- test/unit/data_test.js
+++ test/unit/data_test.js
@@ -8,19 +8,27 @@ import XFlowValidator from '../../lib/xflow-validator';
 chai.use(chaiAsPromised);
 
 var goodFlowFiles = [
+  'data/10_steps.json',
+  'data/arithmetic_addition.json',
   'data/branch_boolean.json',
+  'data/branch_boolean_and_expressions_return.json',
+  'data/branch_boolean_condition.json',
   'data/create_object.json',
-  'data/arithmetic_addition.json'
+  'data/loop_5x.json'
 ];
 
 var badFlowFiles = [
-  'data/bad_flows/multiple_entry_nodes.json'
+  // 'data/bad_flows/bad_capabilities.json', // XXX: Bad cap matches are still schema-valid
+  'data/bad_flows/identical_id_nodes.json',
+  'data/bad_flows/multiple_entry_nodes.json',
+  'data/bad_flows/no_entry_nodes.json'
 ];
 
 var allFlowFiles = goodFlowFiles.concat(badFlowFiles);
 
 var schemaFile = 'data/xflow-schema.json';
-var schemaJSON = fs.readFileSync(schemaFile, 'utf-8');
+var schemaData = fs.readFileSync(schemaFile, 'utf-8');
+var schemaJSON = JSON.parse(schemaData);
 
 describe('JSON data ', function() {
   it(' all flows should validate ', function() {
@@ -36,6 +44,17 @@ describe('JSON data ', function() {
 });
 
 describe('JSON data - graph validation ', function() {
+
+    it('should not validate arbitrary JSON ', function() {
+
+      var json = { 'x': 'y' };
+      var res  = tv4.validate(json, schemaJSON);
+      expect(res).to.equal(false);
+
+      res  = (new XFlowValidator(json, schemaJSON)).validate();
+      expect(res).to.equal(false);
+
+    });
 
   it(' all good flows should validate ', function() {
     goodFlowFiles.forEach(function(file) {
@@ -53,6 +72,7 @@ describe('JSON data - graph validation ', function() {
       var res  = (new XFlowValidator(json, schemaJSON)).validate();
       expect(res).to.equal(false);
     });
+
   });
 
 });

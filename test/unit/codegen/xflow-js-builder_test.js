a6ca16eded6ac5e46b9d0b0819ea834b476d67bd
Michiel Kalkman <michiel@nosuchtype.com>
2015-04-16 14:11:33 +0200

    Inital AST and code generation

---
+++ test/unit/codegen/xflow-js-builder_test.js
@@ -0,0 +1,35 @@
+import chai           from 'chai';
+import chaiAsPromised from 'chai-as-promised';
+import fs             from 'fs';
+import vm             from 'vm';
+
+chai.use(chaiAsPromised);
+
+import XFlow           from '../../../lib/xflow';
+import XFlowDispatcher from '../../../lib/xflow-dispatcher';
+import XFlowJSBuilder  from '../../../lib/codegen/xflow-js-builder';
+
+function getXFlow(json, params) {
+  var dispatcher = new XFlowDispatcher();
+  return new XFlow(json, params, dispatcher);
+}
+
+describe('XFlowJSBuilder basic', function() {
+
+  it('loads a json flow and compiles to JS', function() {
+
+    var data = fs.readFileSync('data/flows/create_object.json', 'utf-8');
+    var json = JSON.parse(data);
+    var res = (getXFlow(json, {})).start();
+    expect(res).to.deep.equal([{}]);
+
+    var builder  = new XFlowJSBuilder(json);
+    var jscode   = builder.generate();
+    var vmResult = vm.runInThisContext(jscode);
+    expect(vmResult).to.deep.equal(res);
+    // expect(jscode).to.deep.equal(res);
+
+  });
+
+});
+

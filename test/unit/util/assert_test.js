348def1618cb8e3235c76e6a7f9d19050f77d043
Michiel Kalkman <michiel@nosuchtype.com>
2015-04-22 08:28:42 +0200

    Add isFunction and length helpers, asserts, tests

--- test/unit/util/assert_test.js
+++ test/unit/util/assert_test.js
@@ -147,6 +147,76 @@ describe('Assertion utils ', function() {
 
   });
 
+  describe('isFunction', function() {
+    it('should correctly assert if a property is a Function', function() {
+      var assertEmptyError = function() {
+        AssertUtil.isFunction();
+      };
+      var assertIncorrectError = function() {
+        AssertUtil.isFunction(1);
+      };
+      var assertOk = function() {
+        AssertUtil.isFunction(function() {});
+      };
+      expect(assertEmptyError).to.throw(Error);
+      expect(assertIncorrectError).to.throw(Error);
+      expect(assertOk).to.not.throw(Error);
+    });
+
+  });
+
+  describe('length', function() {
+    it('should correctly assert the length of a property', function() {
+      /* jshint maxstatements:25 */
+      var assertEmptyError = function() {
+        AssertUtil.length();
+      };
+      var assertIncorrectError = function() {
+        AssertUtil.length(1);
+      };
+
+      var assertOk1 = function() {
+        AssertUtil.length([], 0);
+      };
+      var assertOk2 = function() {
+        AssertUtil.length([1,2], 2);
+      };
+      var assertOk3 = function() {
+        AssertUtil.length('', 0);
+      };
+      var assertOk4 = function() {
+        AssertUtil.length('123', 3);
+      };
+
+      var assertNOK1 = function() {
+        AssertUtil.length([], 2);
+      };
+      var assertNOK2 = function() {
+        AssertUtil.length([1,2], 1);
+      };
+      var assertNOK3 = function() {
+        AssertUtil.length('123', 4);
+      };
+      var assertNOK4 = function() {
+        AssertUtil.length('123', 2);
+      };
+
+      expect(assertEmptyError).to.throw(Error);
+      expect(assertIncorrectError).to.throw(Error);
+
+      expect(assertOk1).to.not.throw(Error);
+      expect(assertOk2).to.not.throw(Error);
+      expect(assertOk3).to.not.throw(Error);
+      expect(assertOk4).to.not.throw(Error);
+
+      expect(assertNOK1).to.throw(Error);
+      expect(assertNOK2).to.throw(Error);
+      expect(assertNOK3).to.throw(Error);
+      expect(assertNOK4).to.throw(Error);
+    });
+
+  });
+
 
 });
 

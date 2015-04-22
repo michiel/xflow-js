d0e1e6fb5652306924795340ac06803906ab9ecd
Michiel Kalkman <michiel@nosuchtype.com>
2015-04-22 17:30:07 +0200

    Add first test case with flox eval

--- test/unit/codegen/xflow-js-builder_test.js
+++ test/unit/codegen/xflow-js-builder_test.js
@@ -9,6 +9,8 @@ import XFlow           from '../../../lib/xflow';
 import XFlowDispatcher from '../../../lib/xflow-dispatcher';
 import XFlowJSBuilder  from '../../../lib/codegen/xflow-js-builder';
 
+import Flox            from '../../../lib/flox';
+
 function getXFlow(json, params) {
   var dispatcher = new XFlowDispatcher();
   return new XFlow(json, params, dispatcher);
@@ -17,10 +19,26 @@ function getXFlow(json, params) {
 function buildScript(json) {
   var builder  = new XFlowJSBuilder(json);
   var jscode   = builder.generate();
-  console.log('JSCODE ', jscode);
+  // console.log('JSCODE ', jscode);
   return new vm.Script(jscode, {
     displayErrors : true
   });
+}
+
+function getEnv() {
+  return {
+    scope : {
+      CounterValue : 0,
+      LoopValue    : false
+    },
+    Flox  : {
+      'evalexpr' : function(exp, scope) {
+        // console.log('EXP ', exp, 'SCOPE ', scope);
+        var str = Flox.substituteExpression(exp, scope);
+        return Flox.parse(str);
+      }
+    }
+  };
 }
 
 function loadJson(path) {
@@ -63,19 +81,22 @@ describe('XFlowJSBuilder basic', function() {
   it('loads a json flow with a flox expression and compiles to JS', function() {
 
     var json = loadJson('data/flows/loop_5x.json');
-    var skope = {
-      scope : {},
-      Flox  : {
-        'evalexpr' : function() {return 5;}
-      }
-    };
-    var res = (getXFlow(json, {})).start();
+    var skope = getEnv();
+//     skope.scope = {
+//       CounterValue : 0,
+//       LoopValue    : 0
+//     };
+    // var res = (getXFlow(json, {})).start();
     // expect(res).to.deep.equal([{}]);
 
+    var out = [{
+      CounterValue : 6,
+      LoopValue    : true
+    }];
     var script   = buildScript(json);
     var ctxt     = vm.createContext(skope);
     var vmResult = script.runInNewContext(ctxt);
-    expect(vmResult).to.deep.equal(res);
+    expect(vmResult).to.deep.equal(out);
   });
 
 });

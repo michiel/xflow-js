344e26b0eaa1c035cbf9f7c755174b3bf015a4ff
Michiel Kalkman <michiel@nosuchtype.com>
2015-05-15 16:08:42 +0200

    Rename inconsistent branch.branch to branch.edge

--- data/schemas/xflow-schema.js
+++ data/schemas/xflow-schema.js
@@ -1,247 +1,248 @@
 export default {
-  "$schema": "http://nosuchtype.com/xflow/v1/schema#",
-  "id": "/",
-  "type": "object",
-  "properties": {
-    "id": {
-      "id": "id",
-      "type": "string",
-      "minLength": 1
+  '$schema': 'http://nosuchtype.com/xflow/v1/schema#',
+  'id': '/',
+  'type': 'object',
+  'properties': {
+    'id': {
+      'id': 'id',
+      'type': 'string',
+      'minLength': 1
     },
-    "name": {
-      "id": "name",
-      "type": "string",
-      "minLength": 1
+    'name': {
+      'id': 'name',
+      'type': 'string',
+      'minLength': 1
     },
-    "version": {
-      "id": "version",
-      "type": "number",
-      "minimum": 0
+    'version': {
+      'id': 'version',
+      'type': 'number',
+      'minimum': 0
     },
-    "requirements": {
-      "id": "requirements",
-      "type": "array",
-      "minItems": 1,
-      "uniqueItems": true,
-      "additionalItems": true,
-      "items": {
-        "type": "object",
-        "properties": {
-          "xtype": {
-            "id": "xtype",
-            "type": "string",
-            "minLength": 1
+    'requirements': {
+      'id': 'requirements',
+      'type': 'array',
+      'minItems': 1,
+      'uniqueItems': true,
+      'additionalItems': true,
+      'items': {
+        'type': 'object',
+        'properties': {
+          'xtype': {
+            'id': 'xtype',
+            'type': 'string',
+            'minLength': 1
           },
-          "version": {
-            "id": "version",
-            "type": "integer",
-            "minimum": 1,
-            "default": 1
+          'version': {
+            'id': 'version',
+            'type': 'integer',
+            'minimum': 1,
+            'default': 1
           }
         },
-        "additionalProperties": false
+        'additionalProperties': false
       }
     },
-    "variables": {
-      "id": "variables",
-      "type": "object",
-      "properties": {
-        "in": {
-          "type": "array",
-          "minItems": 0,
-          "uniqueItems": true,
-          "additionalItems": true,
-          "items": {
-            "type" : "object",
-            "oneOf" : [
-              { "$ref": "#/definitions/scopeVariable" }
+    'variables': {
+      'id': 'variables',
+      'type': 'object',
+      'properties': {
+        'in': {
+          'type': 'array',
+          'minItems': 0,
+          'uniqueItems': true,
+          'additionalItems': true,
+          'items': {
+            'type' : 'object',
+            'oneOf' : [
+              { '$ref': '#/definitions/scopeVariable' }
             ]
           }
         },
-        "out": {
-          "type": "array",
-          "minItems": 0,
-          "uniqueItems": false,
-          "additionalItems": true,
-          "items": {
-            "type": "object",
-            "oneOf" : [
-              { "$ref": "#/definitions/scopeVariable" }
+        'out': {
+          'type': 'array',
+          'minItems': 0,
+          'uniqueItems': false,
+          'additionalItems': true,
+          'items': {
+            'type': 'object',
+            'oneOf' : [
+              { '$ref': '#/definitions/scopeVariable' }
             ]
           }
         },
-        "local": {
-          "id": "local",
-          "type": "array",
-          "minItems": 0,
-          "uniqueItems": true,
-          "additionalItems": true,
-          "items": {
-            "type": "object",
-            "oneOf" : [
-              { "$ref": "#/definitions/scopeVariable" }
+        'local': {
+          'id': 'local',
+          'type': 'array',
+          'minItems': 0,
+          'uniqueItems': true,
+          'additionalItems': true,
+          'items': {
+            'type': 'object',
+            'oneOf' : [
+              { '$ref': '#/definitions/scopeVariable' }
             ]
           }
         }
       },
-      "additionalProperties": false,
-      "required" : [
-        "in",
-        "out",
-        "local"
+      'additionalProperties': false,
+      'required' : [
+        'in',
+        'out',
+        'local'
       ]
     },
-    "nodes": {
-      "id": "nodes",
-      "type": "array",
-      "minItems": 2,
-      "uniqueItems": true,
-      "additionalItems": true,
-      "items": {
-        "type": "object",
-        "oneOf" : [
-          { "$ref": "#/definitions/flowNode" }
+    'nodes': {
+      'id': 'nodes',
+      'type': 'array',
+      'minItems': 2,
+      'uniqueItems': true,
+      'additionalItems': true,
+      'items': {
+        'type': 'object',
+        'oneOf' : [
+          { '$ref': '#/definitions/flowNode' }
         ]
       }
     },
-    "edges": {
-      "id": "edges",
-      "type": "array",
-      "minItems": 1,
-      "uniqueItems": true,
-      "additionalItems": true,
-      "items": {
-        "type": "array",
-        "oneOf" : [
-          { "$ref": "#/definitions/flowEdge" }
+    'edges': {
+      'id': 'edges',
+      'type': 'array',
+      'minItems': 1,
+      'uniqueItems': true,
+      'additionalItems': true,
+      'items': {
+        'type': 'array',
+        'oneOf' : [
+          { '$ref': '#/definitions/flowEdge' }
         ]
       }
     },
-    "branches": {
-      "id": "branches",
-      "type": "array",
-      "minItems": 0,
-      "uniqueItems": true,
-      "additionalItems": true,
-      "items": {
-        "type": "object",
-        "oneOf" : [
-          { "$ref": "#/definitions/flowBranch" }
+    'branches': {
+      'id': 'branches',
+      'type': 'array',
+      'minItems': 0,
+      'uniqueItems': true,
+      'additionalItems': true,
+      'items': {
+        'type': 'object',
+        'oneOf' : [
+          { '$ref': '#/definitions/flowBranch' }
         ]
       }
     }
   },
-  "definitions" : {
-    "scopeVariable" : {
-      "properties" : {
-        "name" : {
-          "type": "string",
-          "minLength": 1
+  'definitions' : {
+    'scopeVariable' : {
+      'properties' : {
+        'name' : {
+          'type': 'string',
+          'minLength': 1
         },
-        "vtype" : {
-          "type": "string",
-          "minLength": 1,
-          "default": "string",
-          "enum": [
-            "boolean",
-            "number",
-            "string"
+        'vtype' : {
+          'type': 'string',
+          'minLength': 1,
+          'default': 'string',
+          'enum': [
+            'boolean',
+            'number',
+            'string'
           ]
         },
-        "value" : {
-          "type": "object",
-          "anyOf": [
-            { "type": "boolean" },
-            { "type": "string" },
-            { "type": "number" }
+        'value' : {
+          'type': 'object',
+          'anyOf': [
+            { 'type': 'boolean' },
+            { 'type': 'string' },
+            { 'type': 'number' }
           ]
         }
       },
-      "additionalProperties" : false,
-      "required": [
-        "name",
-        "vtype"
+      'additionalProperties' : false,
+      'required': [
+        'name',
+        'vtype'
       ]
     },
-    "flowNode" : {
-      "properties" : {
-        "id" : {
-          "type" : "number"
+    'flowNode' : {
+      'properties' : {
+        'id' : {
+          'type' : 'number'
         },
-        "nodetype" : {
-          "type" : "string"
+        'nodetype' : {
+          'type' : 'string'
         },
-        "action" : {
-          "type" : "string"
+        'action' : {
+          'type' : 'string'
         },
-        "label" : {
-          "type" : "string"
+        'label' : {
+          'type' : 'string'
         },
-        "parameters" : {
-          "type" : "object"
+        'parameters' : {
+          'type' : 'object'
         }
       },
-      "additionalProperties" : false,
-      "required": [
-        "id",
-        "nodetype",
-        "action",
-        "label",
-        "parameters"
+      'additionalProperties' : false,
+      'required': [
+        'id',
+        'nodetype',
+        'action',
+        'label',
+        'parameters'
       ]
     },
-    "flowEdge" : {
-      "properties" : {
-        "type": "array",
-        "minItems": 2,
-        "maxItems": 2,
-        "uniqueItems": true,
-        "additionalItems": false,
-        "items": {
-          "type": "number"
+    'flowEdge' : {
+      'properties' : {
+        'type': 'array',
+        'minItems': 2,
+        'maxItems': 2,
+        'uniqueItems': true,
+        'additionalItems': false,
+        'items': {
+          'type': 'number'
         },
-        "additionalProperties": false
+        'additionalProperties': false
       }
     },
-    "flowBranch" : {
-      "properties" : {
-        "branch" : {
-          "type": "object",
-          "oneOf" : [
-            { "$ref": "#/definitions/flowBranch" }
+    'flowBranch' : {
+      'properties' : {
+        'edge' : {
+          'type': 'object',
+          'oneOf' : [
+            { '$ref': '#/definitions/flowEdge' }
           ]
         },
-        "name" : {
-          "type" : "string"
+        'name' : {
+          'type' : 'string'
         },
-        "value" : {
-          "type": "object",
-          "anyOf": [
-            { "type": "boolean" },
-            { "type": "string" },
-            { "type": "number" }
+        'value' : {
+          'type': 'object',
+          'anyOf': [
+            { 'type': 'boolean' },
+            { 'type': 'string' },
+            { 'type': 'number' }
           ]
         }
       },
-      "additionalProperties" : false,
-      "required": [
-        "branch",
-        "name",
-        "value"
+      'additionalProperties' : false,
+      'required': [
+        'edge',
+        'name',
+        'value'
       ]
     }
   },
-  "additionalProperties": false,
-  "required": [
-    "id",
-    "name",
-    "version",
-    "requirements",
-    "variables",
-    "nodes",
-    "edges",
-    "branches"
+  'additionalProperties': false,
+  'required': [
+    'id',
+    'name',
+    'version',
+    'requirements',
+    'variables',
+    'nodes',
+    'edges',
+    'branches'
   ]
-}
+};
+
 
 

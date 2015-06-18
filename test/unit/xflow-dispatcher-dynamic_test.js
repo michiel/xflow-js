80eb23b70b6161934219a3633eb5103d1f901ad9
Michiel Kalkman <michiel@nosuchtype.com>
2015-06-18 20:18:02 +1200

    Add dispatcher-dynamic tests for coverage

--- test/unit/xflow-dispatcher-dynamic_test.js
+++ test/unit/xflow-dispatcher-dynamic_test.js
@@ -66,4 +66,118 @@ describe('XFlowDispatcherDynamic [sync]', function() {
 
 });
 
+describe('XFlowDispatcherDynamic [async]', function() {
 
+  it('loads and executes a flow', function() {
+    var data = fs.readFileSync('data/flows/create_object.json', 'utf-8');
+    var json = JSON.parse(data);
+    var res  = getXFlow(json, {
+      'ReturnValue' : true
+    }).startQ();
+    expect(res).to.eventually.deep.equal({
+      'ReturnValue' : true
+    });
+  });
+
+  it('runs a flow with a boolean expression followed by a branch', function() {
+    var data = fs.readFileSync('data/flows/branch_boolean_condition.json', 'utf-8');
+    var json = JSON.parse(data);
+    var res  = (getXFlow(json, {
+      'CalcValueA' : 1,
+      'CalcValueB' : 2
+    })).startQ();
+    expect(res).to.eventually.deep.equal({
+      'ReturnValue' : true
+    });
+  });
+
+});
+
+
+describe('XFlowDispatcherDynamic invalid dispatchers', function() {
+
+  it('should error on invalid dispatchers', function() {
+    var dispatcher;
+
+    dispatcher = new XFlowDispatcherDynamic({});
+    expect(function() {
+      dispatcher.addDispatcher(null);
+    }).to.throw(Error);
+
+    dispatcher = new XFlowDispatcherDynamic({});
+    expect(function() {
+      dispatcher.addDispatcher('test', null);
+    }).to.throw(Error);
+
+    dispatcher = new XFlowDispatcherDynamic({});
+    expect(function() {
+      dispatcher.addDispatcher('test', {
+      });
+    }).to.throw(Error);
+
+    dispatcher = new XFlowDispatcherDynamic({});
+    expect(function() {
+      dispatcher.addDispatcher('test', {
+        dispatch : function() {}
+      });
+    }).to.throw(Error);
+
+    dispatcher = new XFlowDispatcherDynamic({});
+    expect(function() {
+      dispatcher.addDispatcher('test', {
+        dispatchQ : function() {},
+        dispatch  : function() {}
+      });
+    }).to.throw(Error);
+
+    dispatcher = new XFlowDispatcherDynamic({});
+    expect(function() {
+      dispatcher.addDispatcher('test', {
+        version   : 1,
+        dispatchQ : function() {},
+        dispatch  : function() {}
+      });
+    }).to.not.throw(Error);
+
+    dispatcher = new XFlowDispatcherDynamic({});
+    expect(function() {
+      dispatcher.addDispatcher('test', {
+        version   : 1,
+        dispatchQ : function() {},
+        dispatch  : function() {}
+      });
+      dispatcher.addDispatcher('test', {
+        version   : 1,
+        dispatchQ : function() {},
+        dispatch  : function() {}
+      });
+    }).to.throw(Error);
+
+  });
+});
+
+
+describe('XFlowDispatcherDynamic request for unavailable dispatcher', function() {
+
+  it('should error on unavailable dispatchers', function() {
+    var dispatcher;
+
+    dispatcher = new XFlowDispatcherDynamic({});
+
+    expect(function() {
+      dispatcher.processNode({
+        nodetype   : 'unavailable',
+        parameters : {}
+      }, {});
+    }).to.throw(Error);
+
+    expect(function() {
+      dispatcher.processNodeQ({
+        nodetype   : 'unavailable',
+        parameters : {}
+      }, {});
+    }).to.throw(Error);
+
+  });
+
+});

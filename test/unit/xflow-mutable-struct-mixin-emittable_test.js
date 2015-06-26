ee82efd45a9220cc29efca0b1422dc07ca4fd9b0
Michiel Kalkman <michiel@nosuchtype.com>
2015-06-26 19:47:11 +1000

    Move emittable mixes test to separate files

---
+++ test/unit/xflow-mutable-struct-mixin-emittable_test.js
@@ -0,0 +1,43 @@
+import chai           from 'chai';
+import chaiAsPromised from 'chai-as-promised';
+import fs             from 'fs';
+import RSVP           from 'rsvp';
+
+chai.use(chaiAsPromised);
+
+import XFlowMutableStruct  from '../../lib/xflow-mutable-struct';
+
+function getJSON(path) {
+  var data = fs.readFileSync(path, 'utf-8');
+
+  try {
+    JSON.parse(data);
+  } catch(e) {
+    console.log('Cannot parse JSON - The offending file is : ', path);
+    throw new Error('Cannot parse JSON - The offending file is : ' + path);
+  }
+
+  return JSON.parse(data);
+}
+
+describe('XFlowMutableStruct emissions', function() {
+
+  it('can emit events', function() {
+    var json      = getJSON('data/flows/10_steps.json');
+    var xf        = new XFlowMutableStruct(json);
+    var defer     = RSVP.defer();
+
+    xf.on('change', function(deltas) {
+      expect(deltas[0].op).to.equal('add');
+      expect(deltas[0].path).to.equal('/nodes/10');
+      defer.resolve();
+    });
+
+    var node = xf.newNode();
+    xf.addNode(node);
+
+    return defer.promise;
+
+  });
+
+});
--- test/unit/xflow-mutable-struct_test.js
+++ test/unit/xflow-mutable-struct_test.js
@@ -1,7 +1,6 @@
 import chai           from 'chai';
 import chaiAsPromised from 'chai-as-promised';
 import fs             from 'fs';
-import RSVP           from 'rsvp';
 
 chai.use(chaiAsPromised);
 
@@ -340,25 +339,4 @@ describe('XFlowMutableStruct ', function() {
 
 });
 
-describe('XFlowMutableStruct emissions', function() {
-
-  it('can emit events', function() {
-    var json      = getXFlowJSON('data/flows/10_steps.json');
-    var xf        = new XFlowMutableStruct(json);
-    var defer     = RSVP.defer();
-
-    xf.on('change', function(deltas) {
-      expect(deltas[0].op).to.equal('add');
-      expect(deltas[0].path).to.equal('/nodes/10');
-      defer.resolve();
-    });
-
-    var node = xf.newNode();
-    xf.addNode(node);
-
-    return defer.promise;
-
-  });
-
-});
 
